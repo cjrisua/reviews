e41efa8b66721c3241cf8738cce1ca0bc5c7c70d
Carlos Rivera <carisua@verizon.net>
2020-12-22 11:27:48 -0500

    command terrori

--- wine/management/commands/terroir.py
+++ wine/management/commands/terroir.py
@@ -8,10 +8,17 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         countryAlias = [{'United States':['US','United States','USA']}]
 
-        df  = pd.read_csv("terroir.csv")
-        print(f"Dataset Size {df.shape[0]}")
-        for name, group in df.groupby('country'):
+        df_terroir  = pd.read_csv("terroir.csv")
+        df_country  = pd.read_csv("country_code.csv")
+
+        print(f"Dataset Size {df_terroir.shape[0]}")
+        for name, group in df_terroir.groupby('country'):
             #producer, created = Producer.objects.get_or_create(slug=slugify(name), defaults={'name': name})
             has_alias = next(filter(lambda x: slugify(name) in [slugify(f) for f in x.values()][0], countryAlias),None)
             country_name = name if has_alias is None else list(has_alias.keys())[0]
-            country, created = Country.objects.get_or_create(slug=slugify(name), defaults={'name': name, 'productionrank':0, 'abbreviation':''})
+            pd_country = df_country[df_country.Country == country_name]
+            country, created = Country.objects.get_or_create(slug=slugify(country_name), 
+                    defaults={  'name': name, 
+                                'productionrank':pd_country.Rank.values[0], 
+                                'abbreviation':pd_country.Alpha3.values[0]})
+            
